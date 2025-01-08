81d677173381af7b6437b0f40d61e6e2c6405d9a
Alon Yoffe <alonyoffe1@gmail.com>
2025-01-08 13:47:58 +0200

    Search

--- NetworkingProject/Controllers/HomePageController.cs
+++ NetworkingProject/Controllers/HomePageController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NetworkingProject.Models;
 
 namespace AudioBookStore.Controllers
 {
     public class HomePageController : Controller
     {
+        private readonly BookRepository _bookRepository;
+
+        public HomePageController()
+        {
+            string connectionString = "Server=localhost;Database=NetProj_Web_db;Trusted_Connection=True;";
+            _bookRepository = new BookRepository(connectionString);
+        }
+
         // Action for the main page
         public ActionResult Index()
         {
@@ -24,9 +33,23 @@ namespace AudioBookStore.Controllers
         [HttpGet]
         public ActionResult Search(string query)
         {
-            ViewBag.Query = query ?? "No query provided"; // Show "No query provided" if the search bar is empty.
-            ViewBag.Message = "Search functionality is under development.";
-            return View(); // Looks for Views/HomePage/Search.cshtml
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Convert query to lowercase for case-insensitive search
+            query = query.ToLower();
+
+            // Get books from database where title contains the search query
+            List<BookModel> books = _bookRepository.GetAllBooks()
+                .Where(b => b.Title.ToLower().Contains(query) ||
+                            b.Author.ToLower().Contains(query) ||
+                            b.Publisher.ToLower().Contains(query))
+                .ToList();
+
+            // Pass the books to the view
+            return View(books);
         }
     }
 }
