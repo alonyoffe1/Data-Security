2faeb64c93395e42a205e0e19b1734b0c5d9eba5
alonyoffe1 <83630476+alonyoffe1@users.noreply.github.com>
2025-04-18 11:51:32 +0300

    Add files via upload

---
+++ NetworkingProject/Models/ResetPasswordModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetworkingProject.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
